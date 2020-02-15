693f881a6c2dbceea710bed11d88c26158b9a1b7
Glitch (dom-chulsuk) <none>
2020-02-15 18:31:36 +0000

    🕍🏔 Updated with Glitch

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# dom_chulsuk
+# 트위치 감성돔회 출석봇
+
+채팅을 친 순서대로 유저들의 명단을 작성해주고
+방송종료 전에 명단을 이메일로 보내주는 채팅봇
+
+## 명령어
+
+!출석시작 : 입력한 시점부터 유저들은 카운트함. ignore_list에 있는 계정들은 받아적지않고, 한번 입력한 유저는 두번받아적지않음
+
+!출석끝 : 지정된 이메일로 출석부명단을 보내줌.
+
+해당 명령어들은 방송을 송출중인 스트리머 본인만 실행가능한 명령어임.
+
+### Environment Variables
+ 
+| *Variable*  | *Description*   |
+|---|---|---|---|---|
+| `BOT_USERNAME`  |  The account (username) that the chatbot uses to send chat messages. This can be your Twitch account. Alternately, many developers choose to create a second Twitch account for their bot, so it's clear from whom the messages originate. |  
+|`CHANNEL_NAME`   |  The Twitch channel name where you want to run the bot. Usually this is your main Twitch account.
+|`OAUTH_TOKEN`   |The token to authenticate your chatbot with Twitch's servers. Generate this with [https://twitchapps.com/tmi/](https://twitchapps.com/tmi/) (a Twitch community-driven wrapper around the Twitch API), while logged in to your chatbot account. The token will be an alphanumeric string.|  
+|`EMAIL_NAME` | 메일을 보내는 계정(gmail) |
+|`EMAIL_PW` | 메일을 보내는 계정의 비밀번호 |
+|`RECEIVeR_EMAIL` | 출석부를 받을 이메일주소 |
+
+
+### 주의
+
+현재 보내는 이메일은 gmail만 설정가능.
+
+gmail에서 보안수준이 낮은 앱의 액세스를 허용해야 사용가능하므로, 가계정을 만들어서 사용하는 것을 추천함
---
+++ bot.js
@@ -0,0 +1,104 @@
+const tmi = require('tmi.js');
+const nodemailer = require('nodemailer');
+const smtpPool = require('nodemailer-smtp-pool');
+
+// Define configuration options
+const opts = {
+  identity: {
+    username: process.env.BOT_USERNAME,
+    password: process.env.OAUTH_TOKEN
+  },
+  channels: [
+    process.env.CHANNEL_NAME
+  ]
+};
+
+// Create a client with our options
+const client = new tmi.client(opts);
+
+// Register our event handlers (defined below)
+client.on('message', onMessageHandler);
+client.on('connected', onConnectedHandler);
+
+// Connect to Twitch:
+client.connect();
+
+const smtpTransport = nodemailer.createTransport(
+  smtpPool({
+    service: 'Gmail',
+    host: 'localhost',
+    port: '465',
+    tls: {
+      rejectUnauthorized: false,
+    },
+
+    //이메일 전송을 위해 필요한 인증정보
+
+    //gmail 계정과 암호
+    auth: {
+      user: process.env.EMAIL_NAME,
+      pass: process.env.EMAIL_PW,
+    },
+  })
+);
+
+var ignore_list = [
+  process.env.CHANNEL_NAME,
+  'Bbangddeock',
+  'Nightbot',
+];
+var user_list = [];
+var chulSuk = false;
+
+// Called every time a message comes in
+function onMessageHandler (target, context, msg, self) {
+  if (self) { return; } // Ignore messages from the bot
+
+  // Remove whitespace from chat message
+  const commandName = msg.trim();
+
+  // If the command is known, let's execute it
+  if (commandName === '!출석시작' && context.username === ignore_list[0] && chulSuk === false) { // 방장만 가능한 명령어
+    chulSuk = true;
+    user_list = [];
+    client.say(target, `[출석리셋] 출석표가 초기화 되었습니다.`);
+    console.log(`* Executed ${commandName} command`);
+  } else if(commandName === '!출석끝' && context.username === ignore_list[0] && chulSuk === true){ // 방장만 가능한 명령어
+    let todayList = '';
+    for(let i=0;i<user_list.length;i++){
+      todayList = `${todayList}${i+1}. ${user_list[i]}<br>`
+    }
+    
+    const mailOpt = {
+      from: process.env.EMAIL_NAME,
+      to: process.env.RECEIVER_EMAIL,
+      subject: '금일의 출석표',
+      html: todayList,
+    };
+    smtpTransport.sendMail(mailOpt, function(err, res) {
+      if (err) {
+        console.log(err);
+        client.say(target,'띠용 뭔가 잘못되고있어 ㅠ_ㅠ');
+        //여기다가 채팅으로 입력 코드
+      } else {
+        console.log('Message send :' + res);
+        //client.say(target,`${process.env.EMAIL_NAME}으로 출석표를 보내놨습니다 ㅎㅎ`);
+        client.say(target,'출석표를 보내놨습니다 ㅎㅎ');
+        chulSuk = false;
+      }
+    });
+  }
+  else if(chulSuk === true && ignore_list.indexOf(context.username) === -1) { // ignore_list에 없는 유저만 읽기
+    const usrName = context['display-name'];
+    if(user_list.indexOf(usrName) === -1){
+      user_list.push(usrName);
+      //client.say(target, `${usrName} 님 반갑습니다. 데스노트적힘 ㅅㄱ`);
+    }
+
+  }
+}
+
+// Called every time the bot connects to Twitch chat
+function onConnectedHandler (addr, port) {
+  console.log(`* Connected to ${addr}:${port}`);
+}
