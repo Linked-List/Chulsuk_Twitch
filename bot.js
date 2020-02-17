0c67cdd613dadf792d206282577b15bf177adfa3
Linked-List <null>
2020-02-17 18:05:26 +0000

    nohup

--- bot.js
+++ bot.js
@@ -10,11 +10,9 @@ dotenv.config();
 const opts = {
   identity: {
     username: process.env.BOT_USERNAME,
-    password: process.env.OAUTH_TOKEN
+    password: process.env.OAUTH_TOKEN,
   },
-  channels: [
-    process.env.CHANNEL_NAME
-  ]
+  channels: [process.env.CHANNEL_NAME],
 };
 
 // Create a client with our options
@@ -46,32 +44,32 @@ const smtpTransport = nodemailer.createTransport(
   })
 );
 
-var ignore_list = [
-  process.env.CHANNEL_NAME,
-  'bbangddeock',
-  'Nightbot',
-];
+var ignore_list = [process.env.CHANNEL_NAME, 'bbangddeock', 'Nightbot'];
 var user_list = [];
 var chulSuk = false;
 
 // Called every time a message comes in
-function onMessageHandler (target, context, msg, self) {
-  if (self) { return; } // Ignore messages from the bot
-
+function onMessageHandler(target, context, msg, self) {
+  if (self) {
+    return;
+  } // Ignore messages from the bot
+	
   // Remove whitespace from chat message
   const commandName = msg.trim();
 
   // If the command is known, let's execute it
-  if (commandName === '!출석시작' && context.username === ignore_list[0] && chulSuk === false) { // 방장만 가능한 명령어
+  if(commandName === '!출석시작' && context.username === ignore_list[0] && chulSuk === false) {
+    // 방장만 가능한 명령어
     chulSuk = true;
     user_list = [];
     client.say(target, `[출석리셋] 출석표가 초기화 되었습니다.`);
-  } else if(commandName === '!출석끝' && context.username === ignore_list[0] && chulSuk === true){ // 방장만 가능한 명령어
+  } else if (commandName === '!출석끝' && context.username === ignore_list[0] && chulSuk === true) {
+    // 방장만 가능한 명령어
     let todayList = '';
-    for(let i=0;i<user_list.length;i++){
-      todayList = `${todayList}${i+1}. ${user_list[i]}<br>`
+    for (let i = 0; i < user_list.length; i++) {
+      todayList = `${todayList}${i + 1}. ${user_list[i]}<br>`;
     }
-    
+
     const mailOpt = {
       from: process.env.EMAIL_NAME,
       to: process.env.RECEIVER_EMAIL,
@@ -81,32 +79,36 @@ function onMessageHandler (target, context, msg, self) {
     smtpTransport.sendMail(mailOpt, function(err, res) {
       if (err) {
         console.log(err);
-        client.say(target,'띠용 뭔가 잘못되고있어 ㅠ_ㅠ 대충 백업은 했는데 링크드불러바');
-        fs.writeFile('./chulSuk.txt',todayList.replace('<br>','\n'),function(err){
-			if(err){
-				console.log(`Error ${err}`);
-			}
-			console.log('파일에 대신 저장했습니다');
-		});
+        client.say(
+          target,
+          '띠용 뭔가 잘못되고있어 ㅠ_ㅠ 대충 백업은 했는데 링크드불러바'
+        );
+        fs.writeFile('./chulSuk.txt', todayList.replace('<br>', '\n'), function(
+          err
+        ) {
+          if (err) {
+            console.log(`Error ${err}`);
+          }
+          console.log('파일에 대신 저장했습니다');
+        });
       } else {
         console.log('Message send :' + res);
-        client.say(target,'출석표를 보내놨습니다 ㅎㅎ');
+        client.say(target, '출석표를 보내놨습니다 ㅎㅎ');
         chulSuk = false;
       }
     });
-  }
-  else if(chulSuk === true && ignore_list.indexOf(context.username) === -1) { // ignore_list에 없는 유저만 읽기
+  } else if (chulSuk === true && ignore_list.indexOf(context.username) === -1) {
+    // ignore_list에 없는 유저만 읽기
     const usrName = context['display-name'];
-    if(user_list.indexOf(usrName) === -1){
+    if (user_list.indexOf(usrName) === -1) {
       user_list.push(usrName);
       console.log(`Pushed ${usrName} to ChulsukBoo`);
       //client.say(target, `${usrName} 님 반갑습니다. 데스노트적힘 ㅅㄱ`);
     }
-
   }
 }
 
 // Called every time the bot connects to Twitch chat
-function onConnectedHandler (addr, port) {
+function onConnectedHandler(addr, port) {
   console.log(`* Connected to ${addr}:${port}`);
 }
